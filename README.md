19b04d7a7b5152f34fa094474607c4066f89751c
Didac Bigorda <didacbigorda@gmail.com>
2019-07-24 16:42:31 -0400

    First version. CORS fetch problem

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fahrenheit to Celsius converter with a WebAssembly function
+
+Simple Fahrenheit to Celsius converter website built with web assembly.
---
+++ dev/c/fahrToCels.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (int argc, char ** argv)
+{
+  printf("Web Assembly!\n");
+}
+
+int fahrToCels(int fahr)
+{
+  return 5 * (fahr - 32) / 9;
+}
