b37520107a8972fd66e13e7d462e5614040baea7
Didac Bigorda <didacbigorda@gmail.com>
2019-07-24 16:57:33 -0400

    Add form to website

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+import { grab, element, DOMScrubber } from "./utils/DOM/DOM";
+
 'use strict'
 
-((window, document) => {
-  document.getElementById('body').appendChild()
-})(window, document)
+(() => {
+  grab('body').append(element({
+    tagName: 'form',
+    attributes: {
+      id: 'fahr-form',
+      class: 'fahr-form',
+    },
+    innerHTML: [
+      {
+        tagName: 'input',
+        attributes: {
+          id: 'fahr-input',
+          class: 'search-bar',
+          type: 'text',
+          placeholder: 'Input Fahrenheit temperature'
+        }
+      },
+      {
+        tagName: 'button',
+        attributes: {
+          id: 'button',
+          class: 'btn search-btn'
+        },
+        text: 'Convert'
+      }
+    ]
+  }));
+})()
