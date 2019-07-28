dae52150470df667806fce44767d5a0cc6198a08
Didac Bigorda <didacbigorda@gmail.com>
2019-07-28 14:09:12 -0400

    Changes to gulp config

--- build.ts
+++ build.ts
@@ -5,11 +5,15 @@ import * as less from "gulp-less";
 let browserSync = require('browser-sync').create();
 let reload      = browserSync.reload;
 
-gulp.task("build", async () => {
+gulp.task("compile-c", async () => {
   const data = await Service.compileFile(project.getFile("src/main.c"), "c", "wasm", "-g -O3");
   const outWasm = project.newFile("docs/out/main.wasm", "wasm", true);
   outWasm.setData(data);
 });
+
+gulp.task("watch-c", async () => {
+  gulp.watch("./src/**/*.c", ["compile-c"]);
+})
 
 gulp.task("compile-less", function() {  
   gulp.src('./src/styles.less')
@@ -23,13 +27,14 @@ gulp.task("watch-less", function() {
 
 gulp.task("serve", function () {
   browserSync.init({
-      server: {
-          baseDir: "./docs/"
-      }
+    server: {
+        baseDir: "./docs/"
+    }
   }); 
   gulp.watch("./src/*.less").on("change", reload);
+  gulp.watch("./src/*.c").on("change", reload);
   gulp.watch("./docs/*.html").on("change", reload);
   gulp.watch("./docs/*.js").on("change", reload);
 })
 
-gulp.task("default", ["build", "watch-less", "serve"], async () => {});
+gulp.task("default", ["watch-less", "watch-c", "serve"], async () => {});
