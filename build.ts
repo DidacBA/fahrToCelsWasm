e0edec079490f8e39c181653d5ba2396204b3f84
Didac Bigorda <didacbigorda@gmail.com>
2019-07-28 13:57:57 -0400

    Add less support

--- build.ts
+++ build.ts
@@ -1,5 +1,9 @@
 import * as gulp from "gulp";
 import { Service, project } from "@wasm/studio-utils";
+import * as less from "gulp-less";
+
+let browserSync = require('browser-sync').create();
+let reload      = browserSync.reload;
 
 gulp.task("build", async () => {
   const data = await Service.compileFile(project.getFile("src/main.c"), "c", "wasm", "-g -O3");
@@ -7,4 +11,25 @@ gulp.task("build", async () => {
   outWasm.setData(data);
 });
 
-gulp.task("default", ["build"], async () => {});
+gulp.task("compile-less", function() {  
+  gulp.src('./src/styles.less')
+    .pipe(less())
+    .pipe(gulp.dest('./docs/css/'));
+}); 
+
+gulp.task("watch-less", function() {  
+  gulp.watch('./src/**/*.less' , ['compile-less']);
+});
+
+gulp.task("serve", function () {
+  browserSync.init({
+      server: {
+          baseDir: "./docs/"
+      }
+  }); 
+  gulp.watch("./src/*.less").on("change", reload);
+  gulp.watch("./docs/*.html").on("change", reload);
+  gulp.watch("./docs/*.js").on("change", reload);
+})
+
+gulp.task("default", ["build", "watch-less", "serve"], async () => {});
